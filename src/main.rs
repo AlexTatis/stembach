d7edb511e0d9fe74113813b7724edd48781fc619
AlexTatis <alejandrocampoh@gmail.com>
2024-06-14 17:47:56 +0200

    Added font rendering

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,73 @@
+use std::{
+    collections::HashMap,
+    fs::{self},
+};
+
 use macroquad::prelude::*;
+use square::Square;
 
-#[derive(Debug, Clone, Copy)]
-struct Rectangle {
-    x: f32,
-    y: f32,
-    w: f32,
-    h: f32,
-    color: Color,
+mod square;
+
+const UNIT_LENGTH: f32 = 40.0; // Real pixels of every square
+
+struct Font {
+    json: HashMap<String, Vec<u8>>,
 }
 
-impl Rectangle {
-    fn draw(&self) {
-        draw_rectangle(
-            self.x,
-            screen_height() - self.y - self.h,
-            self.w,
-            self.h,
-            self.color,
-        )
-    }
-
-    fn new(x: f32, y: f32, w: f32, h: f32, color: Color) -> Self {
-        Self { x, y, w, h, color }
-    }
-
-    fn rotate(&mut self, angle: f32) {
-
-        let v_center = Vec2::new(self.x + self.w / 2.0, self.y + self.h / 2.0);
-        let rotation = Affine2::from_angle_translation(angle, v_center);
-
-        let v = rotation.transform_point2(Vec2::new(self.x, self.y) - v_center);
-
-        self.x = v.x;
-        self.y = v.y;
-    }
-
-    fn translate(&mut self, v: Vec2) {
-        self.x += v.x;
-        self.y += v.y;
-    }
-
-    fn scale(&mut self, n: f32) {
-        self.w *= n;
-        self.h *= n;
-    }
-}
-
-fn transform_1(rectangle: &mut Rectangle) {
-    rectangle.translate(Vec2::new(0.0, rectangle.h));
-    rectangle.rotate(45.0);
-}
-
+impl Font {}
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
+    let text = String::from("BI-Gas");
+
+    let file = fs::read_to_string("monogram-bitmap.json").expect("[!]: Font not found");
+    let font: HashMap<String, Vec<u8>> =
+        serde_json::from_str(file.as_str()).expect("[!]: Error on json desirialization");
+
     loop {
         clear_background(BLACK);
 
-        let mut union_set = vec![Rectangle::new(0.0, 0.0, 60.0, 60.0, GREEN)];
+        let mut union_set = vec![square::Square::new(
+            0.0,
+            0.0,
+            UNIT_LENGTH * 6.0 * text.chars().count() as f32,
+            0.0,
+            GREEN,
+        )];
 
-        for set in &mut union_set {
-            transform_1(set);
+        for _i in 0..2 {
+            
+            let mut tmp_set: Vec<Square> = vec![];
+            
+            for set in &mut union_set {
+                set.scale(1.0 / (6.0 * text.chars().count() as f32));
+
+                for (char_index, character) in text.chars().enumerate() {
+                    let bitmap: &Vec<u8> = font
+                        .get(&character.to_string())
+                        .expect("[!]: Character not recognized. Check the string given");
+
+                    for (row_index, bit) in bitmap.into_iter().enumerate().rev() {
+                        for column_index in 0..5 {
+                            if (bit >> column_index & 1) != 0 {
+                                let mut tmp = set.clone();
+
+                                tmp.translate(Vec2::new(
+                                    UNIT_LENGTH * (column_index + char_index * 7) as f32,
+                                    UNIT_LENGTH * (12 - row_index) as f32,
+                                ));
+
+                                tmp_set.push(tmp);
+                            }
+                        }
+                    }
+                }
+            }
+        union_set = tmp_set;
         }
 
-        for set in union_set {
-            set.draw();
-        }
+
+        union_set.into_iter().for_each(|x| x.draw());
 
         draw_text(
             "IFS Attractor - Alejandro Campo & Álvaro Vázquez",
