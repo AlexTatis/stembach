f72a0c33e3c385f547a82077ffd2369748c8b9cf
AlexTatis <alejandrocampoh@gmail.com>
2024-06-15 14:37:40 +0200

    Added multithreading via rayon

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use std::{collections::HashMap, fs, sync::{Arc, Mutex}};
+use std::{
+    collections::HashMap,
+    fs, sync::{Arc, Mutex},
+};
 
 use macroquad::prelude::*;
+use rayon::prelude::*;
 use square::Square;
 
 mod square;
-mod thread_pool;
-
-use thread_pool::ThreadPool;
 
 const UNIT_LENGTH: f32 = 50.0; // Real pixels of every square
 
-#[macroquad::main("IFS Attractor")]
+#[macroquad::main("BasicShapes")]
 async fn main() {
-    let pool = ThreadPool::new(16);
-
     let text = String::from("A");
     let scale_factor = 6.0 * text.chars().count() as f32; // Factor used for scaling every set into the current iteration font size
 
@@ -32,46 +31,32 @@ async fn main() {
             GREEN,
         )];
 
-        for i in 0..4 {
+        for i in 0..5 {
             let tmp_set: Arc<Mutex<Vec<Square>>> = Arc::new(Mutex::new(vec![]));
 
-            let union_set_arc = Arc::new(Mutex::new(union_set));
+            union_set.par_iter_mut().for_each(|set|for (char_index, character) in text.chars().enumerate() {
+                let bitmap: &Vec<u8> = font
+                    .get(&character.to_string())
+                    .expect("[!]: Character not recognized. Check the string given");
 
-            for set in &mut union_set {
-                let tmp_set_clone = Arc::clone(&tmp_set);
-                let set_clone = set.clone();
-                let text_clone = text.clone();
-                let font_clone = font.clone();
+                for (row_index, bit) in bitmap.into_iter().enumerate().rev() {
+                    for column_index in 0..5 {
+                        if (bit >> column_index & 1) != 0 {
+                            let mut tmp = set.clone();
 
-                pool.execute(move || {
-                    for (char_index, character) in text.chars().enumerate() {
-                        let bitmap: &Vec<u8> = font
-                            .get(&character.to_string())
-                            .expect("[!]: Character not recognized. Check the string given");
+                            tmp.scale(1.0 / scale_factor);
 
-                        for (row_index, bit) in bitmap.into_iter().enumerate().rev() {
-                            for column_index in 0..5 {
-                                if (bit >> column_index & 1) != 0 {
-                                    let mut tmp = set.clone();
+                            tmp.translate(Vec2::new(
+                                UNIT_LENGTH / scale_factor.powi(i)
+                                    * (column_index + char_index * 7) as f32,
+                                UNIT_LENGTH / scale_factor.powi(i) * (12 - row_index) as f32,
+                            ));
 
-                                    tmp.scale(1.0 / scale_factor);
-
-                                    tmp.translate(Vec2::new(
-                                        UNIT_LENGTH / scale_factor.powi(i)
-                                            * (column_index + char_index * 7) as f32,
-                                        UNIT_LENGTH / scale_factor.powi(i)
-                                            * (12 - row_index) as f32,
-                                    ));
-
-                                    tmp_set_clone.lock().unwrap().push(tmp);
-                                }
-                            }
+                            tmp_set.lock().unwrap().push(tmp);
                         }
                     }
-                })
-            }
-
-            println!("{}", tmp_set.lock().unwrap().len());
+                }
+            });
 
             union_set = Arc::try_unwrap(tmp_set).unwrap().into_inner().unwrap();
         }
